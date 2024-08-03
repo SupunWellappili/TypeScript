a4a44d127d29591e5038e14ffd69b53cbf067bac
SupunWellappili <supunchandana1814@gmail.com>
2024-08-03 13:55:29 +0530

    Variable Created for TS

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 
+/*
 import hello from "./hello";
 
 hello(); 
+*/
+
+let frontEndCoder:string = "Frontend Coder";
+frontEndCoder ='ABCD';
+
+const frontEndCoder01:string = "Frontend Coder - 01" 
+//frontEndCoder01 = 'YXYZ'; //
